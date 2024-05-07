0aeaa0cfa083ea005395337ff780a58172da18c3
Mai Thaher <maithaher79@gmail.com>
2024-05-07 03:54:34 +0300

    edit front-end for employees and stock pages including the add-form

--- my_app/views.py
+++ my_app/views.py
@@ -10,68 +10,65 @@ def display_homepage(request):
     return render(request, 'sign-in.html')
 
 def index(request):
-    return render(request , 'index.html' )
+    if 'manager_id' in request.session:
+        return render(request , 'index.html')
+    else:
+        return redirect('/')
+    
 
 def sign_up(request):
     return render(request , 'sign-up.html' )
 
-
-
-
-
-
-
-
-
-
-
 # if anyone want to Sign is
 def sign_in(request):
-    # if he is an EMPLOYEE
-    if request.POST['account_type'] == "1" : # Emoployee
-        errors = models.Employee.objects.login_employee_validator(request.POST)
-        if len(errors) > 0:
-            for key, value in errors.items():
-                messages.error(request, value )
-            return redirect('/')
-        else:
-            employee_email = request.POST['email'] # here we get the email thet ENSERTED
-            employee_password = request.POST['password'] # here we get the password thet ENSERTED
-            employee = models.Employee.objects.filter(email=employee_email) # here we get the EMPLOYEE by the email from DB
-            if employee: # here we check if the EMPLOYEE exist
-                employee_user = employee[0] # here we get the EMPLOYEE from the list
-                if bcrypt.checkpw(employee_password.encode(), employee_user.password.encode()): # here we chick the password 
-                    request.session['employee_id'] = employee_user.id
-                    return redirect('/index')
-                else:
-                    messages.error(request, "Incorrect Password")
-                    # messages.error(request, value , extra_tags = 'admin_login' )
-                    return redirect('/')
-            messages.error(request, "Email is incorrect")
-            return redirect('/')
-    # if he is a MANAGER
-    else:
-        if request.POST['account_type'] == "2" : # Manager
-            errors = models.Manager.objects.login_manager_validator(request.POST)
+    if request.method == 'POST':
+        # if he is an EMPLOYEE
+        if request.POST['account_type'] == "1" : # Emoployee
+            errors = models.Employee.objects.login_employee_validator(request.POST)
             if len(errors) > 0:
                 for key, value in errors.items():
                     messages.error(request, value )
                 return redirect('/')
             else:
-                manager_email = request.POST['email'] # here we get the email thet ENSERTED
-                manager_password = request.POST['password'] # here we get the password thet ENSERTED
-                manager = models.Manager.objects.filter(email=manager_email) # here we get the MANAGER by the email from DB
-                if manager: # here we check if the MANAGER exist
-                    manager_user = manager[0] # here we get the MANAGER from the list
-                    #ADDING MANAGER FROME ADMIN
-                    if manager_password == manager_user.password :
-                        request.session['manager_id'] = manager_user.id
-                        return redirect('/index')
+                employee_email = request.POST['email'] # here we get the email thet ENSERTED
+                employee_password = request.POST['password'] # here we get the password thet ENSERTED
+                employee = models.Employee.objects.filter(email=employee_email) # here we get the EMPLOYEE by the email from DB
+                if employee: # here we check if the EMPLOYEE exist
+                    employee_user = employee[0] # here we get the EMPLOYEE from the list
+                    if bcrypt.checkpw(employee_password.encode(), employee_user.password.encode()): # here we chick the password 
+                        request.session['employee_id'] = employee_user.id
+                        return redirect('/employye_dashboard')
                     else:
                         messages.error(request, "Incorrect Password")
                         # messages.error(request, value , extra_tags = 'admin_login' )
                         return redirect('/')
                 messages.error(request, "Email is incorrect")
+                return redirect('/')
+        # if he is a MANAGER
+        else:
+            if request.POST['account_type'] == "2" : # Manager
+                errors = models.Manager.objects.login_manager_validator(request.POST)
+                if len(errors) > 0:
+                    for key, value in errors.items():
+                        messages.error(request, value )
+                    return redirect('/')
+                else:
+                    manager_email = request.POST['email'] # here we get the email thet ENSERTED
+                    manager_password = request.POST['password'] # here we get the password thet ENSERTED
+                    manager = models.Manager.objects.filter(email=manager_email) # here we get the MANAGER by the email from DB
+                    if manager: # here we check if the MANAGER exist
+                        manager_user = manager[0] # here we get the MANAGER from the list
+                        #ADDING MANAGER FROME ADMIN
+                        if manager_password == manager_user.password :
+                            request.session['manager_id'] = manager_user.id
+                            return redirect('/index')
+                        else:
+                            messages.error(request, "Incorrect Password")
+                            # messages.error(request, value , extra_tags = 'admin_login' )
+                            return redirect('/')
+                    messages.error(request, "Email is incorrect")
+    else:
+        return redirect('/')
 
 
 
@@ -86,3 +83,19 @@ def sign_in(request):
                 # messages.error(request, "Email is incorrect")
                 # return redirect('/')
     return redirect('/')
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
+
+def display_employees(request):
+    return render (request, 'profile.html')
+
+def add_neww_employee(request):
+    pass
+
+def display_employee_dashboard(request):
+    return render(request, 'tables.html')
+
+def add_new_product(request):
+    pass
